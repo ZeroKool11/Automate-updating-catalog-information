599e0048837b2f4419d4196e9b2bbb0d4f3cbfed
Dany Alejandro Silva <perseo.11@gmail.com>
2020-10-02 22:02:21 +0000

    Transform the dicitonary to a json structure.

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 import os
 import requests
 import locale
+import json
 
 url = "http://35.202.181.107/fruits"
 source_img = "supplier-data/images"
@@ -13,8 +14,17 @@ for file in os.listdir(source_desc):
 	name, ext = os.path.splitext(file)
 	if ext == ".txt":
 		with open(source_desc + "/" + file) as fo:
+			
+			# Save in a dictionary structure the content of the file	
 			dic["name"] = fo.readline().strip()
 			dic["weight"] = fo.readline().strip().strip(" lbs")
 			dic["description"] = fo.readline().strip()
 			dic["image_name"] = file
+
+			# Transform the dictionary structure to a json structure
+			json_struc = json.dumps(dic)
+			break
+
+print(json_struc)
+	
 			
