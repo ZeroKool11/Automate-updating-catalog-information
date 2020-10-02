fbd2194e9b98c2742dab21ff24f0e16c83b7f466
Dany Alejandro Silva <perseo.11@gmail.com>
2020-10-02 21:54:40 +0000

    Convert the content of each txt file to a dictionary structure.

--- run.py
+++ run.py
@@ -2,12 +2,19 @@
 
 import os
 import requests
+import locale
 
 url = "http://35.202.181.107/fruits"
 source_img = "supplier-data/images"
 source_desc = "supplier-data/descriptions"
 
 for file in os.listdir(source_desc):
+	dic = {}
 	name, ext = os.path.splitext(file)
 	if ext == ".txt":
-		print(file)
+		with open(source_desc + "/" + file) as fo:
+			dic["name"] = fo.readline().strip()
+			dic["weight"] = fo.readline().strip().strip(" lbs")
+			dic["description"] = fo.readline().strip()
+			dic["image_name"] = file
+			
