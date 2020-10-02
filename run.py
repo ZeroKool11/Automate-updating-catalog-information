fe5b5ae6c56e1ca0b48c6bfcf05aadeae01fc1bd
Dany Alejandro Silva <perseo.11@gmail.com>
2020-10-02 22:41:41 +0000

    Upload the images

--- run.py
+++ run.py
@@ -5,26 +5,27 @@ import requests
 import locale
 import json
 
-url = "http://35.202.181.107/fruits"
-source_img = "supplier-data/images"
-source_desc = "supplier-data/descriptions"
+url = "http://35.202.181.107/fruits/"
+source_img = "supplier-data/images/"
+source_desc = "supplier-data/descriptions/"
+locale.setlocale(locale.LC_ALL, 'en_US.UTF8')
 
 for file in os.listdir(source_desc):
 	dic = {}
 	name, ext = os.path.splitext(file)
 	if ext == ".txt":
-		with open(source_desc + "/" + file) as fo:
+		with open(source_desc + file) as fo:
 			
 			# Save in a dictionary structure the content of the file	
 			dic["name"] = fo.readline().strip()
 			dic["weight"] = fo.readline().strip().strip(" lbs")
 			dic["description"] = fo.readline().strip()
-			dic["image_name"] = file
+			dic["image_name"] = name + ".jpeg"
 
-			# Transform the dictionary structure to a json structure
-			json_struc = json.dumps(dic)
-			break
+			# Upload the data
+			r = requests.post(url, json = dic)
+			r.raise_for_status()
+print(r.status_code)	
 
-print(json_struc)
 	
 			
