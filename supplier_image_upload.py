6e6d27dfcfdeb911d06fa42a803fcdfe29ca0134
Dany Alejandro Silva Sanchez <perseo.11@gmail.com>
2020-10-02 02:38:44 +0000

    Finishing the scrit that uploads the images.

--- supplier_image_upload.py
+++ supplier_image_upload.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 
+import requests
+import os
 
+url = "http://localhost/upload/"
+source = "supplier-data/images"
+
+for file in os.listdir(source):
+	name, ext = os.path.splitext(file)
+	if ext == ".jpeg":
+		with open(source + '/' + file , 'rb') as opened:
+    			r = requests.post(url, files={'file': opened})
